81d8be8cfd83e92d0d339057613be72352084212
connieDeng <dengconnie0723@gmail.com>
2020-02-18 18:55:55 -0500

    shows items as struck through

--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -5,27 +5,61 @@ class Customers extends Component {
   constructor() {
     super();
     this.state = {
-      customers: []
+      customers: [],
+      newTask: ""
     };
   }
 
   componentDidMount() {
-    fetch('/api/customers')
+    this.fetchTasks();
+  }
+
+  fetchTasks = () => {
+    fetch('/api/tasks')
       .then(res => res.json())
       .then(customers => this.setState({customers}, () => console.log('Customers fetched...', customers)));
   }
 
+  addingTasks = () => {
+    console.log(this.state.customers);
+    //axios.post('http://localhost:5000/?')
+    let c = this.state.customers;
+    c.push({ "name": this.state.newTask, "completed": false });
+    //reset input value
+    this.setState({
+      newTask: '',
+      customers: c,
+    });
+  }
+
+  update = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value, //newTask = userInput
+    })
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.addingTasks();
+  }
+
+
   render() {
     return (
       <div>
-        <h2>To Do List</h2>
-        <ul>
         {
-        this.state.customers.map(customer => 
-          <li key={customer.name}>{customer.name}</li>
-        )
+          this.state.customers.map((uid, ind) => {
+            let lineStyle;
+            if (uid.completed === true) { lineStyle = 'line-through'; }
+            else { lineStyle = 'none'; }
+
+            return <li key={ind} style={{ textDecorationLine: lineStyle }}>{uid.name}</li>
+          })
         }
-        </ul>
+        <form onSubmit={this.onSubmit}>
+          <input type='text' name='newTask' value={this.state.newTask} onChange={this.update}></input>
+        </form>
+        <button onClick={this.addingTasks}> LOSER </button>
       </div>
     );
   }
